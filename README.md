df18c689f29142762bdf539157531822f5563268
Machoudi <stehat982@gmail.com>
2023-10-19 01:02:52 +0100

    package version 1.0.2

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# Fastail - Simplifying Tailwind CSS Setup
+
+**Fastail** is a powerful Node.js command-line tool that streamlines the process of setting up a project with Tailwind CSS. With a single command, you can quickly initialize your project, install Tailwind CSS and its dependencies, create or update a `tailwind.config.js` file, and generate an `index.css` file with essential Tailwind CSS base styles.
+
+## Key Features
+
+- Initialize a new project effortlessly.
+- Automatically install Tailwind CSS, PostCSS, and Autoprefixer.
+- Set up or update a `tailwind.config.js` file with a basic configuration.
+- Create an `index.css` file with crucial Tailwind CSS base styles. and if it already exists, find it and update it.
+
+## Installation
+
+Ensure you have Node.js and npm installed on your system. To get started with **Fastail**, execute the following command:
+
+```sh
+npx fastail init
+```
+## Note
+
+After initializing your project you can easily uninstall the fastail package and it will not effect your project. use the following command :
+```sh
+npm uninstall fastail
+```
+
+## Contributing
+
+We welcome contributions and improvements to this project. If you'd like to contribute, please open an issue or a pull request.
+
+## Feedback
+
+If you have any questions or feedback, don't hesitate to get in touch. We value your input and would love to hear from you!
+
+Enjoy a fast and efficient setup with Fastail!
+
---
+++ fastail.js
@@ -0,0 +1,127 @@
+#!/usr/bin/env node
+import { exec } from 'child_process';
+import fs from 'fs';
+
+
+function fastail() {
+    console.log("\x1b[36m");
+    function loadingAnimation() {
+      let i = 0;
+      const animationFrames = ['|', '/', '-', '\\'];
+      return setInterval(() => {
+        process.stdout.write(`\rInstalling... ${animationFrames[i % 4]}`);
+        i++;
+      }, 250);
+    }
+
+    console.log('Initializing your project...');
+  // Step 1: Run the commands to initialize Tailwind CSS
+  const command1 = 'npm init -y';
+  exec(command1, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error running command1: ${error}`);
+      return;
+    }
+
+    console.log('Project initialized.');
+    const loadingInterval = loadingAnimation();
+
+    // Step 2: Install Tailwind CSS and related packages
+    const command2 = "npm install -D tailwindcss postcss autoprefixer";
+    exec(command2, (error, stdout, stderr) => {
+      clearInterval(loadingInterval);
+      if (error) {
+        console.error(`Error running command2: ${error}`);
+        return;
+      }
+      console.log('\nTailwind CSS and related packages installed.');
+
+
+      // Step 3: Initialize Tailwind CSS configuration 
+      const command3 = "npx tailwindcss init -p";
+      exec(command3, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error running command3: ${error}`);
+          return;
+        }
+        console.log('tailwind.config.js has been created.');
+
+
+        // Step 4: Check if tailwind.config.js exists
+        fs.access('tailwind.config.js', fs.constants.F_OK, (err) => {
+          if (err) {
+            console.log('tailwind.config.js not found.');
+          } else {
+            console.log('tailwind.config.js found.');
+
+            // Step 5: Read and edit tailwind.config.js
+            fs.readFile('tailwind.config.js', 'utf8', (err, data) => {
+              if (err) {
+                console.error(`Error reading tailwind.config.js: ${err}`);
+              } else {
+                const updatedContent = `/** @type {import('tailwindcss').Config} */
+  export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }`;
+
+                fs.writeFile('tailwind.config.js', updatedContent, (err) => {
+                  if (err) {
+                    console.error(`Error writing to tailwind.config.js: ${err}`);
+                  } else {
+                    console.log('tailwind.config.js has been updated.');
+                    
+                  }
+                });
+
+                // Step 6: Check if index.css exists
+                  fs.access('index.css', fs.constants.F_OK, (indexCssErr) => {
+                      if (indexCssErr) {
+                      // Create index.css if it doesn't exist
+                      const initialIndexCssContent = `@tailwind base;\n@tailwind components;\n@tailwind utilities;`;
+                      fs.writeFile('index.css', initialIndexCssContent, (createErr) => {
+                          if (createErr) {
+                          console.error(`Error creating index.css: ${createErr}`);
+                          } else {
+                          console.log('index.css created.');
+                          }
+                      });
+                      } else {
+                      // Update the content of index.css
+                      const updatedIndexCssContent = `@tailwind base;\n@tailwind components;\n@tailwind utilities;`;
+                      fs.writeFile('index.css', updatedIndexCssContent, (updateErr) => {
+                          if (updateErr) {
+                          console.error(`Error updating index.css: ${updateErr}`);
+                          } else {
+                          console.log('index.css updated.');
+                          }
+                      });
+                      }
+                  });
+                  const note = "\n// IMPORTAND NOTE : IF YOU'RE USING A FRAMEWORK MOVE INDEX.CSS TO THE RIGHT FOLDER\n"
+                  setTimeout(() => {
+                    console.log("\x1b[33m%s\x1b[0m", note);
+                  }, "1000");
+                  setTimeout(() => {
+                    console.log("\x1b[33m%s\x1b[0m");
+                  }, "1001");
+              }
+            });
+          }
+        });
+ 
+      });
+    });
+
+  });
+
+
+}
+
+fastail();
